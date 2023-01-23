f237f755ffac3d5e3c0ba65b38fb83646b7cc536
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-23 19:26:38 +0200

    Add Index view in the views.py

--- todolist_project/views.py
+++ todolist_project/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todolist_project.models import Task
+
+
+class Index(LoginRequiredMixin, generic.ListView):
+    model = Task
+    queryset = Task.objects.all()
+    template_name = "todolist_project/index.html"
