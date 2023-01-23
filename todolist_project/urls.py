ea95ee4a95edab05823557228bb6d7487c298950
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-23 19:18:31 +0200

    Update path for home screen in the todolist_project/urls.py

--- todolist_project/urls.py
+++ todolist_project/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import Index
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", Index.as_view(), name="index")
 ]
 
 app_name = "todolist_project"
