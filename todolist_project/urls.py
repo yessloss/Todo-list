5232fce2ef612a7e419aefc97d4e081780fc15f2
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-24 15:48:15 +0200

    Add all path for the task in urls.py

--- todolist_project/urls.py
+++ todolist_project/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import Index
+from .views import Index, IndexCreateView, IndexUpdateView, IndexDeleteView
 
 urlpatterns = [
-    path("", Index.as_view(), name="index")
+    path("", Index.as_view(), name="index"),
+    path("task/create/", IndexCreateView.as_view(), name="task_create"),
+    path("task/<int:pk>/update/", IndexUpdateView.as_view(), name="task_update"),
+    path("task/<int:pk>/delete/", IndexDeleteView.as_view(), name="task_delete")
 ]
 
 app_name = "todolist_project"
