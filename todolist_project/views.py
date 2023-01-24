23b466c56a1214fa6f7d473b01e1a9f467fc9ed5
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-24 13:54:17 +0200

    Add create and update and delete views for Index

--- todolist_project/views.py
+++ todolist_project/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todolist_project.models import Task
@@ -9,3 +10,21 @@ class Index(LoginRequiredMixin, generic.ListView):
     model = Task
     queryset = Task.objects.filter("datetime").group_by("is_done")
     template_name = "todolist_project/index.html"
+
+
+class IndexCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todolist_project:index")
+
+
+class IndexUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todolist_project:index")
+
+
+class IndexDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todolist_project:index")
